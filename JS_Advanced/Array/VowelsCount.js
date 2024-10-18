24a1e9a17326c165e5392688e2e3d735b2c301d1
jishnuvadakara <jishnuv750@gmail.com>
2024-10-18 07:41:53 +0530

    array question solve

--- JS_Advanced/Array/VowelsCount.js
+++ JS_Advanced/Array/VowelsCount.js
@@ -14,4 +14,22 @@ for (let i = 0; i < str.length; i++) {
     }
   }
 }
-console.log(obj)
+// console.log(obj);
+
+//usig recursion
+
+function getVowels(str, i = 0, obj = {}) {
+  if (i == str.length) {
+    return obj;
+  }
+  if (set.has(str[i])) {
+    if (obj[str[i]]) {
+      obj[str[i]] += 1;
+    } else {
+      obj[str[i]] = 1;
+    }
+  }
+  return getVowels(str, i + 1, obj);
+}
+
+console.log(getVowels(str));
