c3b5221903f354fb570b379f72920c04a0457d17
jishnuvadakara <jishnuv750@gmail.com>
2024-10-24 07:42:03 +0530

    feat:added new algorithm

---
+++ DataStructures_Dive/Sort/bubblesort.js
@@ -0,0 +1,20 @@
+const arr = [5, 23, 54, 54, 5, 79, 25, 111, 1, 135, 6];
+
+const bubblSort = (arr) => {
+  let swapped = true;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped=true
+      }
+    }
+  } while (swapped);
+  return arr
+};
+
+console.log(bubblSort(arr));
