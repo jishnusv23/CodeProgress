6fec51de0f452064e50b1d129256b68e415374ab
jishnuvadakara <jishnuv750@gmail.com>
2024-11-12 07:20:11 +0530

    somethings runing in test folder

--- test/vite-project/src/App.jsx
+++ test/vite-project/src/App.jsx
@@ -1,34 +1,10 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
-import Parent from "./Components/ForwardRef/Parent";
-// import ErrorBoundary from "./Components/ErrorBoundary/ErrorBoundary";
-// import ErrorProneComponent from "./Components/ErrorBoundary/ErrorProneComponent";
-// import UseMemo from "./Components/Workout/UseMemo";
-// import Visible from "./Components/Workout/Visible";
-// import Debouncing from "./Components/Workout/Debouncing";
-// import REfExample from "./Components/Workout/REfExample";
-
-// import Parent from "./Components/Workout/Parent";
-// import Child from "./Components/Workout/Child";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
-      {/* <Parent/>
-    <Child/> */}
-      {/* <REfExample/> */}
-      {/* <Debouncing /> */}
-      {/* <Visible /> */}
-      {/* <UseMemo/> */}
       <h1>App file</h1>
-      {/* <ErrorBoundary>
-        <ErrorProneComponent />
-      </ErrorBoundary> */}
-      <Parent />
+      
     </>
   );
 }
--- test/vite-project/src/Components/Workout/Child.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import UpdateComponent from './UpdateComponent'
-const Child = ({increment,count}) => {
-    return (
-        <button onMouseOver={increment}>MouseEnter:{count}</button>
-    )
-}
-
-export default UpdateComponent(Child);
