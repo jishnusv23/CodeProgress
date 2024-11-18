60dafd7109233013c79e82e0f1c1cd5ec49c6f77
jishnuvadakara <jishnuv750@gmail.com>
2024-11-18 23:20:56 +0530

    add trie and bt

--- DataStructures_Dive/Trie/test.js
+++ DataStructures_Dive/Trie/test.js
@@ -31,7 +31,7 @@ class TestTrie {
     return node.isEnded;
   }
   delete(word) {
-     this._delete(this.root, word, 0);
+    this._delete(this.root, word, 0);
   }
   _delete(node, word, i) {
     if (node == null) {
@@ -64,6 +64,57 @@ class TestTrie {
 
     return result;
   }
+  FindLongestString() {
+    let longest = "";
+    let queue = [[this.root, ""]];
+    while (queue.length) {
+      const [node, word] = queue.shift();
+      if (node.isEnded && word.length > longest.length) {
+        longest = word;
+      }
+      for (let char in node.children) {
+        queue.push([node.children[char], word + char]);
+      }
+    }
+    return longest;
+  }
+  Autocomplete(word) {
+    let node = this.root;
+    for (let char of word) {
+      if (!node.children[char]) {
+        return [];
+      }
+      node = node.children[char];
+    }
+    let list = [];
+    this.CollectWords(node, list, word);
+    return list;
+  }
+  CollectWords(node, list, word) {
+    if (node.isEnded) {
+      list.push(word);
+    }
+    for (let char in node.children) {
+      this.CollectWords(node.children[char], list, word + char);
+    }
+  }
+  ShortestWord() {
+    let shortestWord = "";
+    let queue = [[this.root, ""]];
+    while (queue.length) {
+      const [node, word] = queue.shift();
+      if (
+        node.isEnded &&
+        (shortestWord == "" || word.length < shortestWord.length)
+      ) {
+        shortestWord = word;
+      }
+      for (let char in node.children) {
+        queue.push([node.children[char], word + char]);
+      }
+    }
+    return shortestWord;
+  }
 }
 const trie = new TestTrie();
 
@@ -75,5 +126,8 @@ trie.insert("ball");
 trie.insert("batman");
 console.log(trie.Print());
 console.log(trie.Search("app"));
+console.log(trie.Autocomplete('b'))
+console.log(trie.FindLongestString())
+console.log(trie.ShortestWord())
 // console.log(trie.delete('ball'))
 // console.log(trie.Print());
