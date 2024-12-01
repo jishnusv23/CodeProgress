facb45580dafde5aee4f1ddd5c5f6b8e9656e65a
jishnuvadakara <jishnuv750@gmail.com>
2024-12-01 10:22:44 +0530

    nothing to change everything test

--- test/vite-project/src/App.jsx
+++ test/vite-project/src/App.jsx
@@ -1,18 +1,12 @@
 import "./App.css";
-import ParentForward from "./Components/ParentForward";
-// import Parent from "./Components/Parent";
-// import MakeError from "./Components/MakeError";
-import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
+
 
 function App() {
   return (
     <>
-      <ErrorBoundary>
       <h1>{"Hello"}</h1>
-        {/* <MakeError /> */}
-        {/* <Parent/> */}
-        <ParentForward/>
-      </ErrorBoundary>
+
+  
     </>
   );
 }
